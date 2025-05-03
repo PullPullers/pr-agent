7da7fd35b088fcb6df2ef4f3ccdaed029765d65e
joosomi <wnthal1211@gmail.com>
2025-05-03 17:06:48 +0900

    test: add self-reflection test cases

--- test.py
+++ test.py
@@ -1,1 +1,28 @@
 print("Hello from PR-Agent Test!")
+
+
+def divide(a, b):
+    if b == 0:
+        return 0
+    return a / b
+
+
+def check_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def unused_function():
+    temp = 42
+    # TODO
+    pass
+
+
+def duplicate_logic(x):
+    if x > 10:
+        return "large"
+    if x > 10:
+        return "big"
+    return "small"
